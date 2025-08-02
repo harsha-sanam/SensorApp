288c815b2dd4e13446af14de5ede55cd3926f56f
harsha-sanam <34478540+harsha-sanam@users.noreply.github.com>
2025-08-01 18:12:17 -0700

    temp

--- sensor-ui/environment.ts
+++ sensor-ui/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5287/api' // replace with your actual API base
+  apiUrl: 'http://35.222.164.202:5287/api' // replace with your actual API base
 };
